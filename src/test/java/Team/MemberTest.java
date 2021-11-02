dfd78def2bb9d1c39345933f224817b7f871c294
Tomasz Pielecki <tomasz.pielecki86@gmail.com>
2021-11-02 22:31:28 +0100

    Initial commit

---
+++ src/test/java/Team/MemberTest.java
@@ -0,0 +1,36 @@
+package Team;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Test;
+
+class MemberTest {
+    // testowanie getterow w Javie
+    @org.junit.jupiter.api.Test
+    void getName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getType() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getLvl() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getRank() {
+    }
+    //testowanie zmiennych
+    @Test
+    void name() {
+    }
+    @Test
+    void rank() {
+    }
+    @Test
+    void lvl() {
+    }
+    @Test
+    void type() {
+    }
+}
